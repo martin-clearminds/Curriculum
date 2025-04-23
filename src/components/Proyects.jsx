fce6851e886e61923c98bea0cb70a8dc5bab3d88
martin1906s <martin.simbana007@gamil.com>
2025-04-23 17:20:15 -0500

    Cambio de desarrollo a terminado en proyecto de grado

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from 'next/image';
 const projectsData = [
-    { name: "Neuro Game", image: "/projects/neurogame.svg", description: "Juego de rehabilitación para el desarrollo cognitivoy motriz de niños y adultos mayores.", technologies: ["Python", "Nexts.js", "CSS", "HTML"], link: "https://neurogame.vercel.app/", github: "https://github.com/martin1906s/Proyecto-de-Grado", githubImage: "/skills/github.svg", estado: "En desarrollo", },
+    { name: "Neuro Game", image: "/projects/neurogame.svg", description: "Juego de rehabilitación para el desarrollo cognitivoy motriz de niños y adultos mayores.", technologies: ["Python", "Nexts.js", "CSS", "HTML"], link: "https://neurogame.vercel.app/", github: "https://github.com/martin1906s/Proyecto-de-Grado", githubImage: "/skills/github.svg", estado: "Terminado", },
     { name: "La Mesa Del Chef", image: "/projects/logoColegio.png", description: "Página web para la eleccion de platillos de comidas de un buffet.", technologies: ["React", "Next.js", "CSS", "HTML"], link: "https://mesadelchef.vercel.app/", github: "https://github.com/martin1906s/LaMesaDelChef", githubImage: "/skills/github.svg", estado: "En desarrollo", },
     { name: "Un Planeta Más Limpio es Posible", image: "/projects/logoColegio.png", description: "Página web informativa e interactiva sobre la contaminación ambiental", technologies: ["HTML", "CSS", "JavaScript"], link: "https://proyectointerdisciplinar-9de66.web.app/", github: "https://github.com/martin1906s/Proyecto-Interdisciplinario", githubImage: "/skills/github.svg", estado: "Terminado", }
 ]
